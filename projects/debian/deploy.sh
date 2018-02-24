39c5b55a97cb5c7a4375460315161af096f8c84f
Alan Carson <acarson@cloudsmith.io>
2018-02-24 12:37:25 +0000

    Adding upload to cloudsmith for debian

--- projects/debian/deploy.sh
+++ projects/debian/deploy.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "deploy"
+echo "deploying to Cloudsmith with cloudsmith-cli"
+
+cd "projects/debian/src"
+
+pwd
+
+ls
+
+cloudsmith push deb cloudsmith/examples/ubuntu/trusty cloudsmith-debian-example_1.0_amd64.deb
