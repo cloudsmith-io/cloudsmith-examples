8b2804f5e175105189b85eb2277bc85c08c732b8
Lee Skillen <lskillen@users.noreply.github.com>
2018-02-26 10:53:19 +0000

    Use native Gradle upload

--- projects/gradle/deploy.sh
+++ projects/gradle/deploy.sh
@@ -1,27 +1,4 @@
 #!/bin/bash
 
-echo "deploying to Cloudsmith with cloudsmith-cli"
-
-cd "projects/gradle/src"
-
-
-pwd
-ls
-
-cd "build"
-
-pwd
-ls
-
-cd "poms"
-
-pwd
-ls
-
-cd "-"
-
-
-
-cloudsmith push maven cloudsmith/examples build/libs/cloudsmith-gradle-example-0.0.1-SNAPSHOT.jar --pom-file build/poms/pom-default.xml
-
-#gradle uploadArchives
+echo "deploying to Cloudsmith"
+gradle uploadArchives
