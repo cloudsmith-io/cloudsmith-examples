c91d1295ef652928b8f996b892bd8c07c06e204f
Alan Carson <acarson@cloudsmith.io>
2018-02-27 12:10:34 +0000

    Update to python example

--- projects/python/deploy.sh
+++ projects/python/deploy.sh
@@ -2,14 +2,6 @@
 
 echo "Deploying Cloudsmith Python Example..."
 
-cd "projects/python/src"
+cd "projects/python/src/dist"
 
-pwd
-ls
-
-cd "dist"
-
-pwd
-ls
-
-cloudsmith push python cloudsmith/examples cloudsmith-python-example-1.0-py2.py3-none-any.whl
+cloudsmith push python cloudsmith/examples cloudsmith_python_example-1.0-py2.py3-none-any.whl
