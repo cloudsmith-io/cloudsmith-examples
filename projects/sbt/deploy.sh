e3cadd595ad9fed1648f99521bc62e4fc9b887bb
Alan Carson <acarson@cloudsmith.io>
2018-02-27 18:56:51 +0000

    Add credentials to sbt example

--- projects/sbt/deploy.sh
+++ projects/sbt/deploy.sh
@@ -2,6 +2,13 @@
 
 echo "Deploying Cloudsmith SBT Example"
 
+cat <<EOF >/home/travis/.sbt/credentials
+realm=cloudsmith
+host=api.cloudsmith.io
+user=
+password=${CLOUDSMITH_API_KEY}
+EOF
+
 cd "projects/sbt/src"
 
 sbt 'set credentials += Credentials("cloudsmith", "api.cloudsmith.io", "", "${CLOUDSMITH_API_KEY}")' aetherDeploy
