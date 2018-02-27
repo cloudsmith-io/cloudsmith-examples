bdbe608f54bf41dac809a0fdfe5d3f28c3638a87
Alan Carson <acarson@cloudsmith.io>
2018-02-27 13:49:40 +0000

    Adding dup handling to python

--- projects/python/deploy.sh
+++ projects/python/deploy.sh
@@ -4,4 +4,6 @@ echo "Deploying Cloudsmith Python Example..."
 
 cd "projects/python/src/dist"
 
+mv cloudsmith_python_example-1.0-py2.py3-none-any.whl cloudsmith_python_example-${TRAVIS_BUILD_NUMBER}-1.0-py2.py3-none-any.whl
+
 cloudsmith push python cloudsmith/examples cloudsmith_python_example-1.0-py2.py3-none-any.whl
