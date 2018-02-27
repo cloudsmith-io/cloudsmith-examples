135d68e019d4cdc3657f1597bd251428b6c350e0
Alan Carson <acarson@cloudsmith.io>
2018-02-27 18:37:32 +0000

    Moving to maven wagon deploy

--- projects/maven/deploy.sh
+++ projects/maven/deploy.sh
@@ -4,4 +4,5 @@ echo "... deploying to Cloudsmith with cloudsmith-cli"
 
 cd "projects/maven/src"
 
-cloudsmith push maven cloudsmith/examples target/cloudsmith-maven-example-0.0.1-SNAPSHOT.jar --pom-file pom.xml
+mvn deploy
+#cloudsmith push maven cloudsmith/examples target/cloudsmith-maven-example-0.0.1-SNAPSHOT.jar --pom-file pom.xml
--- projects/sbt/deploy.sh
+++ projects/sbt/deploy.sh
@@ -4,4 +4,4 @@ echo "Deploying Cloudsmith SBT Example"
 
 cd "projects/sbt/src"
 
-sbt aetherDeploy
+sbt 'set credentials += Credentials("cloudsmith", "api.cloudsmith.io", "", "${CLOUDSMITH_API_KEY}")' aetherDeploy
