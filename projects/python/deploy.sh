cbfc13f600914a1a03aa832a2e6e92637bc63d30
Alan Carson <acarson@cloudsmith.io>
2018-02-27 15:11:50 +0000

    Python example tweak

--- projects/python/deploy.sh
+++ projects/python/deploy.sh
@@ -6,4 +6,4 @@ cd "projects/python/src/dist"
 
 mv cloudsmith_python_example-1.0-py2.py3-none-any.whl cloudsmith_python_example-${TRAVIS_BUILD_NUMBER}-1.0-py2.py3-none-any.whl
 
-cloudsmith push python cloudsmith/examples cloudsmith_python_example-1.0-py2.py3-none-any.whl
+cloudsmith push python cloudsmith/examples cloudsmith_python_example-${TRAVIS_BUILD_NUMBER}-1.0-py2.py3-none-any.whl
--- projects/sbt/src/project/plugins.sbt
+++ projects/sbt/src/project/plugins.sbt
@@ -1,5 +1,7 @@
 addSbtPlugin("no.arktekk.sbt" % "aether-deploy" % "0.20.0")
+addMavenResolverPlugin
 
 libraryDependencies += "io.cloudsmith.maven.wagon" % "cloudsmith-maven-wagon" % "0.2.0"
 
 resolvers += Resolver.mavenLocal
+resolvers += "cloudsmith-api" at "https://dl.cloudsmith.io/public/cloudsmith/api/maven"
