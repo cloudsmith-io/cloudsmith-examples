eb1588d787f723117384b1a68d87d186406e8da6
Alan Carson <acarson@cloudsmith.io>
2018-03-25 20:38:32 +0100

    Version update

--- projects/leiningen/src/cloudsmith-leiningen-example/project.clj
+++ projects/leiningen/src/cloudsmith-leiningen-example/project.clj
@@ -1,8 +1,13 @@
-(defproject cloudsmith-leiningen-example "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject cloudsmith-leiningen-example "1.0.0-SNAPSHOT"
+  :description "Cloudsmith Leiningen Example"
+  :url "https://cloudsmith.io"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [lein-cloudsmith-maven-wagon/lein-cloudsmith-maven-wagon "0.1.0"]]
+  :plugins [[lein-cloudsmith-maven-wagon "0.1.0"]]
+  :repositories [["cloudsmith" {:url "https://dl.cloudsmith.io/public/cloudsmith/api/maven"} ]]
   :main cloudsmith-leiningen-example.core
   :aot [cloudsmith-leiningen-example.core])
+  :deploy-repositories [["releases" { :url "cloudsmith-https://api.cloudsmith.io/cloudsmith/examples" :creds :gpg }
+                         "snapshots" { :url "cloudsmith-https://api.cloudsmith.io/cloudsmith/examples" :creds :gpg }]]
