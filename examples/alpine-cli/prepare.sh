7116a5aa0b66398be11e09e76e51ff93c2037f0c
Patrick Carey <pcarey@cloudsmith.io>
2019-03-18 16:04:59 +0000

    Add example build for Alpine Linux

---
+++ examples/alpine-cli/prepare.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eou pipefail
+
+
+# install the cloudsmith CLI
+pip install --user cloudsmith-cli
+
+# replace "__EXAMPLE_VERSION__" with real version number as required
+grep -rl __EXAMPLE_VERSION__ src/ | xargs sed -i "s/__EXAMPLE_VERSION__/1.0.$BUILD_NUMBER/g"
