916fe7d0f19c99cb0bfbc58aa27671d1658e4e9f
Patrick Carey <pcarey@cloudsmith.io>
2019-04-25 16:45:47 +0100

    Add initial example for cargo-cli

---
+++ examples/cargo-cli/prepare.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eou pipefail
+
+
+# install Python and pip
+sudo apt-get update
+sudo apt-get install -y python python-pip
+
+# install the cloudsmith CLI
+pip install --user cloudsmith-cli
+
+# replace "__EXAMPLE_VERSION__" with real version number as required
+grep -rl __EXAMPLE_VERSION__ src/ | xargs sed -i "s/__EXAMPLE_VERSION__/1.0.$BUILD_NUMBER/g"
---
+++ examples/cargo-cli/src/src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("Cloudsmith: Be Awesome. Automate Everything.");
+}
