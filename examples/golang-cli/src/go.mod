6dd5440be6adba6e5367b0ae1426c66a20fa425b
Ian Duffy <iduffy@cloudsmith.io>
2024-12-03 19:09:49 +0000

    Add example golang-cli

---
+++ examples/golang-cli/src/go.mod
@@ -0,0 +1,3 @@
+module github.com/cloudsmith-io/cloudsmith-examples/golang-cli
+
+go 1.21
---
+++ examples/golang-cli/src/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+const Version = "__PLACEHOLDER_VERSION__"
+
+func main() {
+	fmt.Println("Cloudsmith: Be Awesome. Automate Everything.")
+}
